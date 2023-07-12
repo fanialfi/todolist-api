3d39190bf21cdc553686b4a93730d4a3440137a1
fanialfi <fanialfirdaus@gmail.com>
2023-07-13 06:41:25 +0700

    update content type & accept in controllers

--- controllers/todo_controllers.js
+++ controllers/todo_controllers.js
@@ -6,13 +6,24 @@ const controllers = {
     const query = "INSERT INTO todo (nama, keterangan) VALUES($1, $2)";
     const result = await pgConnection(query, [nama, keterangan]);
 
-    res.status(200).json(result);
+    res
+      .status(200)
+      .set({
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      })
+      .json(result);
   },
   getAll: async (req, res) => {
     const query = "SELECT * FROM todo";
     const result = await pgConnection(query);
     res
       .status(200)
+      .set({
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      })
+
       .json({ message: "suksess get all todo", result: result.rows });
   },
 };
